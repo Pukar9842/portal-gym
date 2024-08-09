53159d475cd85f5901d5be7e66647b52a156697c
Pukar9842 <pkarniroula71@gmail.com>
2024-08-09 11:59:23 +0545

    Comitting weiget1

--- src/components/dashboard-default-widgets/Widget1.vue
+++ src/components/dashboard-default-widgets/Widget1.vue
@@ -1,6 +1,8 @@
-
-
-
+<template>
+<div>
+  
+</div>
+</template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
--- src/layouts/default-layout/components/header/Header.vue
+++ src/layouts/default-layout/components/header/Header.vue
@@ -65,7 +65,7 @@
         class="d-flex align-items-stretch justify-content-between flex-lg-grow-1"
         id="kt_app_header_wrapper"
       >
-       <KTHeaderMenu />
+        <KTHeaderMenu />
         <KTHeaderNavbar />
       </div>
     </div>
